8aa424bc5a535c27a8c7a7aad573014cbee0f8e9
Ndema Emmanuel Chidera <ndemaemmanuel82@gmail.com>
2024-08-19 21:55:07 -0700

    helmet csp policy-3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
-})
+  base: "/",
+  server: {
+    hmr: false, 
+    headers: {
+      "Content-Security-Policy": `
+      default-src 'self'; 
+      connect-src 'self' https://quiz-app-backend-cy25.onrender.com http://localhost:* https://*.dial.to/ https://proxy.dial.to/; 
+      img-src 'self' https://media.dscvr.one https://*.dial.to/ https://proxy.dial.to/; 
+      script-src 'self'; 
+      style-src 'self' 'unsafe-inline';
+    `,
+    },
+  },
+});
