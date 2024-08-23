3e9e4bc63f3d4f839430af971332dd16a9d3f6f0
Ndema Emmanuel Chidera <ndemaemmanuel82@gmail.com>
2024-08-23 15:16:24 -0700

    csp issues -3

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  server: {
-    hmr: false,
-    headers: {
-      "Content-Security-Policy": `
-      default-src 'self'; 
-      connect-src 'self' http://localhost:* https://*.dial.to https://proxy.dial.to; 
-      img-src 'self' https://media.dscvr.one https://*.dial.to https://proxy.dial.to; 
-      script-src 'self'  https://quiz-app-backend-cy25.onrender.com; 
-      style-src 'self' 'unsafe-inline';
-    `
-        .replace(/\s{2,}/g, " ")
-        .trim(), // Minimize spaces
-    },
-  },
+ 
+  
 });
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/",
-//   server: {
-//     hmr: false,
-//     headers: {
-//       "Content-Security-Policy": `
-//       default-src 'self';
-//       connect-src 'self' https://quiz-app-backend-cy25.onrender.com http://localhost:* https://*.dial.to/ https://proxy.dial.to/;
-//       img-src 'self' https://media.dscvr.one https://*.dial.to/ https://proxy.dial.to/;
-//       script-src 'self';
-//       style-src 'self' 'unsafe-inline';
-//     `,
-//     },
-//   },
-// });
