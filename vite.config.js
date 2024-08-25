a9066e02151206ffe16eeb202c0bec4197291d7f
Psolite <68122253+psolite@users.noreply.github.com>
2024-08-25 21:10:24 +0200

    CSP fix

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
   base: "/",
 
   server: {
-    hmr: false, // Disable HMR to avoid inline script injection
     headers: {
       'Content-Security-Policy': "connect-src 'self' https://api.dscvr.one https://api1.stg.dscvr.one https://*.helius-rpc.com https://quiz-app-backend-cy25.onrender.com/* https://quiz-app-backend-cy25.onrender.com/api/v1/user/signup "
     }
